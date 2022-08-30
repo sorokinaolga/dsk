69dd7fa30a6c9c1b0ba6f0cbe7889e264dd4a1c6
sorokinaolga <sorokinaop@list.ru>
2022-08-30 15:56:16 +0300

    add feedback popup

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -1,4 +1,4 @@
-// import './modules/jquery.fancybox.min.js';
+import './modules/jquery.fancybox.min.js';
 import './modules/swiper-bundle.min.js';
 // import './modules/jquery-ui.min.js';
 // import './modules/parallax.min.js';
