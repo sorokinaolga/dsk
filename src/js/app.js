1517a40b4c8d76667b5356b012420e329e76ae08
sorokinaolga <sorokinaop@list.ru>
2022-08-31 13:58:46 +0300

    add main slider

--- src/js/app.js
+++ src/js/app.js
@@ -27,55 +27,11 @@ $(document).ready(function () {
   if ($('*').is('.js-promo-slider')) {
     const sliderPromo = new Swiper('.js-promo-slider', {
       slidesPerView: 1,
-      spaceBetween: 50,
-      pagination: {
-        el: ".js-promo-pagination",
-        clickable: true,
+      spaceBetween: 300,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
-    });
-  }
-
-  // brands slider
-  if ($('*').is('.js-brands-slider')) {
-    const sliderBrands = new Swiper('.js-brands-slider', {
-      slidesPerView: 3,
-      spaceBetween: 20,
-      pagination: {
-        el: ".js-brands-pagination",
-        clickable: true,
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 5,
-          spaceBetween: 30
-        },
-        992: {
-          slidesPerView: 7,
-          spaceBetween: 50
-        }
-      }
-    });
-  }
-
-  // partners slider
-  if ($('*').is('.js-partners-slider')) {
-    const sliderPartners = new Swiper('.js-partners-slider', {
-      slidesPerView: 3,
-      spaceBetween: 20,
-      pagination: {
-        el: ".js-partners-pagination",
-        clickable: true,
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 5,
-          spaceBetween: 30
-        },
-        992: {
-          slidesPerView: 7,
-          spaceBetween: 50
-        }
-      }
     });
   }
 });
